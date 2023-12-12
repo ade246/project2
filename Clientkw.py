3bcd216dbbf5252d2b2718d5c801d24b10f33101
Khari Wallace <khari.wallace@dcmail.ca>
2023-12-11 19:42:30 -0500

    First Commit - Added Server and Client Starter files

---
+++ Clientkw.py
@@ -0,0 +1,11 @@
+"""client.py 
+### KHARI WALLACE, 100807131, TPRG 2131 ###
+
+TPRG 2131 Fall 2023 Project 2
+December 14th, 2023
+Khari Wallace <khari.wallace@dcmail.ca>
+
+# This client runs on Pi, sends Pi's 5 arguments from the vcgencmds, plus 50 iterations
+of the data set. sent as Json object.
+"""
+
---
+++ Serverkw.py
@@ -0,0 +1,10 @@
+"""server.py 
+### KHARI WALLACE, 100807131, TPRG 2131 ###
+
+TPRG 2131 Fall 2023 Project 2
+December 14th, 2023
+Khari Wallace <khari.wallace@dcmail.ca>
+
+# This server runs on Pi and Pc, the server acts as a central node that receives,
+processes, and displays data from the connected client
+"""
