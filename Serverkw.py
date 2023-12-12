0c3b0201c7e49fe0bd8a06952ce2ddad21c8c74f
Khari Wallace <khari.wallace@dcmail.ca>
2023-12-11 20:10:07 -0500

    main program with try block

--- Serverkw.py
+++ Serverkw.py
@@ -47,5 +47,46 @@ def handle_client(client_socket, window):
     finally:
         client_socket.close()
 
+def main():
+    window = create_window()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('0.0.0.0', 1500))
+    server_socket.listen(1)
+
+    client_thread = None
+
+    try:
+        while True:
+            event, values = window.read(timeout=10)
+
+            if event in (sg.WIN_CLOSED, 'Exit'):
+                break
+
+            if event == '-TOGGLE_LED-':
+                window['-LED-'].update(text_color='green' if values['-TOGGLE_LED-'] else 'white')
+
+            if event == '-UPDATE_GUI-':
+                data = values['-UPDATE_GUI-']
+                window['-CORE_VOLTAGE-'].update(data["core_voltage"])
+                window['-CORE_TEMP-'].update(data["core_temp"])
+                window['-ARM_MEMORY-'].update(data["arm_memory"])
+                window['-GPU_MEMORY-'].update(data["gpu_memory"])
+                window['-CPU_FREQUENCY-'].update(data["cpu_frequency"])
+                window['-ITERATION-'].update(str(data["iteration"]))
+
+            if not client_thread:
+                client_socket, addr = server_socket.accept()
+                client_thread = threading.Thread(target=handle_client, args=(client_socket, window), daemon=True)
+                client_thread.start()
+
+     except Exception as e:
+        sg.popup_error("Error:", e)
+     finally:
+        server_socket.close()
+        window.close()
+
+if __name__ == '__main__':
+    main()
 
 
